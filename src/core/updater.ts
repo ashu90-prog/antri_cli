b58c896eb7aac282c5dc627009c52ad80045fb53
ashu90-prog <ashuishan9090@gmail.com>
2026-08-18 17:17:19 +0530

    chore(release): bump version to 1.44.0

--- src/core/updater.ts
+++ src/core/updater.ts
@@ -7,7 +7,7 @@ import { log } from '../utils/logger.js';
 const execPromise = util.promisify(exec);
 
 export class Updater {
-  public static readonly CURRENT_VERSION = '1.43.0';
+  public static readonly CURRENT_VERSION = '1.44.0';
   public static readonly PACKAGE_NAME = 'antri_cli';
 
   /**
