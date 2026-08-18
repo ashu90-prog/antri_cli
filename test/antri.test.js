51d52512d0b18ab9617e8948364b6b1af4f8a24e
ashu90-prog <ashuishan9090@gmail.com>
2026-08-18 17:16:19 +0530

    chore(release): bump version to 1.43.0

--- test/antri.test.js
+++ test/antri.test.js
@@ -65,7 +65,7 @@ test('ToolExecutor identifies privacy & security sensitive tools', () => {
 
 test('Updater reports correct package name and current version', () => {
   assert.strictEqual(Updater.PACKAGE_NAME, 'antri_cli');
-  assert.strictEqual(Updater.CURRENT_VERSION, '1.42.0');
+  assert.strictEqual(Updater.CURRENT_VERSION, '1.43.0');
 });
 
 test('GoalLoopEngine initializes with active configuration', () => {
