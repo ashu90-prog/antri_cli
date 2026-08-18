02e1e00c035bfce6a7d8ce471194263eca373f97
ashu90-prog <ashuishan9090@gmail.com>
2026-08-19 00:04:41 +0530

    chore: release v1.49.0

--- test/antri.test.js
+++ test/antri.test.js
@@ -66,7 +66,7 @@ test('ToolExecutor identifies privacy & security sensitive tools', () => {
 
 test('Updater reports correct package name and current version', () => {
   assert.strictEqual(Updater.PACKAGE_NAME, 'antri_cli');
-  assert.strictEqual(Updater.CURRENT_VERSION, '1.48.0');
+  assert.strictEqual(Updater.CURRENT_VERSION, '1.49.0');
 });
 
 test('GoalLoopEngine initializes with active configuration', () => {
